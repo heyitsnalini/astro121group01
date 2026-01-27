f5156c2ec05db5093e69705c707a85753abc6326
heyitsnalini <nalinikumar.devi@gmail.com>
2026-01-26 17:27:11 -0800

    started making sdr object

--- lab1/do_something.py
+++ lab1/do_something.py
@@ -1,2 +1,6 @@
+import ugradio
 import numpy as np
-print(np.arange(10))
+
+#default_sdr = ugradio.sdr.SDR(direct = True,sample_rate = 1e6)
+#aliasing_sdr = ugradio.sdr.SDR(direct = True, fir_coeffs = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
+
