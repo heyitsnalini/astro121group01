c78b198577d25b9b01d7f5bdd8db06009448c98c
heyitsnalini <nalinikumar.devi@gmail.com>
2026-01-27 19:08:59 -0800

    test  data

--- lab1/do_something.py
+++ lab1/do_something.py
@@ -1,6 +1,21 @@
 import ugradio
 import numpy as np
+import matplotlib.pyplot as plt
 
 #default_sdr = ugradio.sdr.SDR(direct = True,sample_rate = 1e6)
 #aliasing_sdr = ugradio.sdr.SDR(direct = True, fir_coeffs = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
+test_sdr = ugradio.sdr.SDR(direct = True,sample_rate = 1e6)
+test_data = test_sdr.capture_data(nblocks =2)[1]
 
+print(test_data)
+
+np.savez("test_data.npz", data = test_data)
+
+npzfile = np.load("test_data.npz")
+
+print(npzfile['data'])
+
+plt.plot(np.arange(len(test_data)), test_data)
+plt.xlim(0, 100)
+plt.ylim(-20, 20)
+plt.show()
